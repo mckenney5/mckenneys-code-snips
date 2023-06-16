c55e2cbcf940afd5626e762bc17f00b5d1b05fdc
mckenney5 <mckenneyadam@gmail.com>
2023-06-16 09:24:39 -0400

    Organized asm folder, added aarch64 example

---
+++ src/asm/aarch64/hello.aarch64.linux.asm
@@ -0,0 +1,29 @@
+/* Hello World for aarch64 (arm64) using Linux System Calls 
+* It is a good idea to use capital X fir registers so you
+* do not confuse it with hex.
+*
+* compile with: as <source> -o name && ld name -o name.out
+*/
+
+.text
+.align 8				// Show we are 64bit
+.global _start
+
+_start:
+	//Print
+	ldr X1, =string			// Load string into memory
+	ldr X2, =len			// Load size of str in memory
+	mov X0, #1			//Set X0 to stdout
+	mov X8, #64			// Linux SysWrite (stdout, str, len)
+	svc #0
+	
+	//Exit
+	eor X0, X0, X0			// Set exit code to zero by xor-ing itself
+	mov X8, #93			// Linux SysExit
+	svc #0
+
+.data
+string:
+	.ascii "Hello, World.\n\0"
+len = . - string
+
